968a2d0e95ac2a89d6ac6477d933a40783f0f64f
Efimov-Alex <fastic2018l@yandex.ru>
2023-06-29 23:20:27 +0300

    attempt 4

--- src/test/java/ru/netology/patient/medical/TestBloodPressure.java
+++ src/test/java/ru/netology/patient/medical/TestBloodPressure.java
@@ -1,3 +1,5 @@
+package ru.netology.patient.medical;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
